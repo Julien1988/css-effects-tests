77fe4c8eb769dc019212113209fed20b30122b28
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-12-17 20:47:44 +0100

    test parrallax effect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [offsetY, setOffsetY] = useState(0)
+  const [offsetX, setOffsetX] = useState(0)
+  const handleScroll = () => {
+    setOffsetY(window.pageYOffset)
+    // if (window.pageYOffset > 1000) {
+    // setOffsetX(window.pageYOffset - 1000)
+    
+    // }
+    setOffsetX(window.pageYOffset)
+  }  
+
+  useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+
+      return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+ 
+ 
+  // Dimension Iphone 6/7/8
+ 
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +34,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className='main bg-amber-300 overflow-hidden '>
+        {console.log(offsetY)}
+        <section className='container bg-amber-300 h-screen' style={{transform: `translateY(-${offsetY * 0.2 }px)`}}>
+          <div className='h-full  flex flex-col justify-center'>
+            <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+         </div>
+        </section>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <section className='container bg-lime-300 h-screen translate-y-80 rotate-45  w-screen' >
+          <div className='h-full  flex flex-col justify-center'>
+          </div>
+        </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <section className='container bg-lime-300 h-screen' >
+          <div className='h-full  flex flex-col  '>
+            <div className='translate-x-3/4'>
+              <h1 className="text-3xl font-bold underline text-center z-10" style={{transform: `translateX(-${offsetX * 0.3}px)`}}>Hi world!</h1>
+            </div>
+          </div>
+        </section>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className='container bg-cyan-300 h-screen overflow-auto ' style={{transform: `translateY(-${offsetY * 0.2 }px)`}}>
+          <div className='h-full  flex flex-col justify-center' >
+            <h1 className="text-3xl font-bold underline text-center">Good Bye !</h1>
+         </div>
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+       
     </div>
   )
 }
